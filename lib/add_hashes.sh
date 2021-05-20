f7de0fd187f5ceb4359c33f6796ec2521566991a
Richard Warburton <richard.warburton@gmail.com>
2021-05-20 17:28:34 +0100

    add in version string into the agent properly

--- lib/add_hashes.sh
+++ lib/add_hashes.sh
@@ -4,7 +4,8 @@ set -eu
 
 export GIT_HASH=$(git rev-parse HEAD)
 export GIT_TAGS=$(git tag --points-at HEAD)
+export VERSION=$(cat version)
 
 cp globals.cpp.in globals.cpp
-sed -i "s/@GIT_HASH@/$GIT_HASH/;s/@GIT_TAGS@/$GIT_TAGS/;" globals.cpp
+sed -i "s/@GIT_HASH@/$GIT_HASH/;s/@GIT_TAGS@/$GIT_TAGS/;s/@AGENT_VERSION@/$VERSION/" globals.cpp
 
