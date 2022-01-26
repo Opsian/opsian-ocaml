8fbc91968c1989efe10f87b36ec994bb742fccf9
Richard Warburton <richard.warburton@gmail.com>
2022-01-26 19:29:39 +0000

    try to force the thread library to be initialized before opsian to avoid races

--- lib/lib_opsian.ml
+++ lib/lib_opsian.ml
@@ -1,6 +1,6 @@
-
 (* Entrypoint into the native code *)
 external start_opsian_native : string -> string -> string -> unit = "start_opsian_native"
 
 let () =
+  let (_ : Thread.t) = Thread.self () in
   start_opsian_native (Sys.ocaml_version) (Sys.executable_name) (Sys.argv.(0))
