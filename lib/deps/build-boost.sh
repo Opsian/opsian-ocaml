18109902f13902be5e1b763680275e8dbeaefecd
Richard Warburton <richard.warburton@gmail.com>
2022-06-10 11:50:06 +0100

    fix release attempt

--- lib/deps/build-boost.sh
+++ lib/deps/build-boost.sh
@@ -4,6 +4,9 @@ set -eux
 
 cd boost
 
+# fix compilation bug on arch / fedora
+sed -i 's/if PTHREAD_STACK_MIN > 0/ifdef PTHREAD_STACK_MIN/' libs/thread/include/boost/thread/pthread/thread_data.hpp
+
 ./bootstrap.sh
 BOOST_ARGS="cxxflags=-fPIC cflags=-fPIC --with-system --with-chrono --with-timer --with-thread --with-date_time --with-regex --with-serialization"
 ./b2 $BOOST_ARGS -d0 -j 6
