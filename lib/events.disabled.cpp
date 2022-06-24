039a4a5ffdb55d5ae8a218b483d65c6282b89466
Richard Warburton <richard.warburton@gmail.com>
2022-06-24 17:49:30 +0100

    fix compilation with runtime_events disabled

--- lib/events.disabled.cpp
+++ lib/events.disabled.cpp
@@ -1,6 +1,10 @@
 
 // Used on platforms where runtime_events isn't available, eg: OCaml < 5.0.0
 
+#include "event_ring_reader.h"
+#include <cstdlib>
+#include "globals.h"
+
 bool has_runtime_events() {
     return false;
 }
@@ -8,6 +12,7 @@ bool enable_runtime_events(const bool wasEnabled, const bool enable) {
     return false;
 }
 uint32_t read_runtime_events(MetricDataListener& listener) {
+    return 0;
 }
 void disable_runtime_events() {
 }
